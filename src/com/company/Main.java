7516be02a52c28b1a92eecf19f9bbeb450d1f3e8
kamalutfulin <kamalutfulin@gmail.com>
2021-03-15 13:44:33 +0300

    modified programm hello java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int count = 1;
         while (true){
-            System.out.println("Hello World! [" + count++ + "]");
+            System.out.println("Hello java! [" + count++ + "]");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException interruptedException) {
